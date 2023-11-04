cc0c5e9018dc5531a0004fc894c4d07a42f42b2f
Rizki <106717697+RizqGyx@users.noreply.github.com>
2023-11-04 19:59:25 +0700

    Fragment Unfinished Change(Java)

--- app/src/main/java/com/example/perac/MainActivity.java
+++ app/src/main/java/com/example/perac/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.perac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -13,8 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Pengaturan Untuk Bagian xml
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.bottomNavigationView.setBackground(null);
+    }
+
+    // Fungsi Untuk Melakukan Perpindahan Fragment Bottom Navbar
+    private void replaceFragment(Fragment fragment){
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.frame_layout, fragment);
+        mFragmentTransaction.addToBackStack(null);
+        mFragmentTransaction.commit();
     }
 }
